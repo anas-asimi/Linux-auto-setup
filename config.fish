c6595518ba1b2fc7f8876e14335d8c2621046f95
Anas Asimi <anas2020cr7@gmail.com>
2024-03-10 22:36:21 +0000

    init

---
+++ config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+	fortune
+end
+
+alias githubFolder="cd /mnt/c/Users/desktopplus/Documents/Github"
+alias w3c_validator="~/w3c_validator.py"
+alias transfer_file="~/transfer_file"
+alias explorer="explorer.exe"
+
