4764ce5db3acb05a74460b68f84d7bd9f8dcbc8f
Anas Asimi <anas2020cr7@gmail.com>
2024-09-11 22:45:00 +0100

    fix

--- clone_all_repos.sh
+++ clone_all_repos.sh
@@ -4,7 +4,7 @@ filename=$(basename "$0")
 limit=100
 
 if [ $# -eq 1 ]; then
-  gh repo list "$0" --limit "$limit" | while read -r repo _; do
+  gh repo list "$1" --limit "$limit" | while read -r repo _; do
     gh repo clone "$repo" "$HOME/$repo"
   done
 else
